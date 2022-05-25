d421481d01b50ee2c36aaa704807697346826fc8
Litsepiso Kose <97793060+litsepiso-kose@users.noreply.github.com>
2022-05-25 13:06:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,86 +1,120 @@
-from flask import Flask, request
-import requests
-from twilio.twiml.messaging_response import MessagingResponse
 
-app = Flask(__name__)
+def decision1():
+    import time
+    print("welcome to Ficksburg Food Court, where we deliver anywhere and anytime between 7am-5pm Monday-Saturday for R25. Choose your actions\n1.Order\n2.Ask for assistance\n3.Delivery status\n4.Payment\n5.Exit ")
 
-@app.route('/', methods=['POST'])
-def whatsbot():
-    incoming_msg = request.values.get('Body', '').lower()
-    resp = MessagingResponse()
-    message = resp.message()
-    responded = False
-
-    greetings = " Welcome to Ficksburg Food Court, where we deliver anywhere and anytime between 9am-4pm Monday-Saturday for R25. Choose your actions\n1.Order\n2.Ask for assistance\n3.Delivery status\n4.Exit\n5.Payments"
-    order =  "Where would you like to order food Wimpy, Imperani, KFC ,Wild BBQ"
-    assistant = "We will connect you to customer service"
-    delivery = "We wil call in 5 minutes to let you know "
-    payments = "We accept cash only"
-    leave =  "Bye see you soon"
-
-    if incoming_msg not in ["1","order","2","ask for assistance","3","delivery status","4","exit","5","payments","wimpy","imperani","kfc","wild bbq"]:
-        message.body(greetings)
-        responded = True
-
-    if incoming_msg == "1" or "order" in incoming_msg:
-        message.body(order)
-        responded = True
-
-    if incoming_msg == "wimpy" in incoming_msg:
-        message.media("https://food-menu-6924.twil.io/Breakfast_Griills_.pdf")
-        responded = True
-
-    if incoming_msg == "kfc" in incoming_msg:
-        message.media("https://food-menu-6924.twil.io/KFC.pdf")
-        responded = True
-
-    if incoming_msg == "imperani" in incoming_msg:
-        message.media("https://food-menu-6924.twil.io/Imperani%20menu.pdf")
-        responded = True
-
-    if incoming_msg == "wild bbq" in incoming_msg:
-        message.media("https://food-menu-6924.twil.io/BBq.jpg")
-        responded = True
-
-    if incoming_msg == "2" or "ask for assistance" in incoming_msg:
-        message.body(assistant)
-        responded = True
-
-    if incoming_msg == "3" or "delivery status" in incoming_msg:
-        message.body(delivery)
-        responded = True
-
-    if incoming_msg == "4" or "exit" in incoming_msg:
-        message.body(leave)
-        responded = True
-        
-    if incoming_msg == "5" or "payments" in incoming_msg:
-        message.body(payments)
-        responded = True
-
-    if not responded:
-        message.body("Sorry I do not understand, type hi to go back to the main menu")
-    return str(resp)
+    customer1 = input()
+    time.sleep(2)
+    ans = 'incorrect'
+    while (ans == 'incorrect'):
+        if (customer1.upper() == "1"):
+            print("Where would you like to eat\n1.Wimpy\n2.Imperani\n3.KFC\n4.Mochachose\n5.Wild BBQ")
+            ans = 'correct'
+            decision2()
+        elif (customer1.upper() == "2"):
+            print("We will connect you to our customer service soon, please wait a bit.")
+            ans = 'correct'
+        elif (customer1.upper() == "3"):
+            print("The driver will contact you to send your delivery status soon")
+            ans = 'correct'
+        elif (customer1.upper() == "4"):
+            print("We accept cash only.")
+            ans = 'correct'
+        elif (customer1.upper() == "5"):
+            print("Thank you for using Ficksburg Food Court.Bye.")
+            ans = 'correct'
+        else:
+            print("Sorry we do not understand")
+            customer1 = input()
 
 
-if __name__ == '__main__':
-    app.run()
+def decision2():
+    import time
+
+    customer1 = input()
+    time.sleep(2)
+    ans = 'incorrect'
+    while (ans == 'incorrect'):
+        if (customer1.upper() == "1"):
+            print("Would you like a menu?\n1.Yes\n2.No")
+            customer1 = input()
+            if (c1.upper() == "1"):
+                print("MENU1")
+                print("Enter your order: ")
+                ans = 'correct'
+                decision3()
+            elif (customer1.upper() == "2"):
+                print("Alright, you may enter your order.")
+                ans = 'correct'
+                decision3()
+        elif (customer1.upper() == "2"):
+            print("Would you like a menu?\n1.Yes\n2.No")
+            customer1 = input()
+            if (customer1.upper() == "1"):
+                print("MENU2")
+                ans = 'correct'
+                decisoin3()
+            elif (customer1.upper() == "2"):
+                print("Alright, you may enter your order.")
+                ans = 'correct'
+                decision3()
+        elif (customer1.upper() == "3"):
+            print("Would you like a menu?\n1.Yes\n2.No")
+            customer1 = input()
+            if (customer1.upper() == "1"):
+                print("MENU3")
+                ans = 'correct'
+                decision3()
+            elif (customer1.upper() == "2"):
+                print("Alright, you may enter your order.")
+                ans = 'correct'
+                decision3()
+        elif (customer1.upper() == "4"):
+            print("Would you like a menu?\n1.Yes\n2.No")
+            customre1 = input()
+            if (customer1.upper() == "1"):
+                print("MENU4")
+                ans = 'correct'
+                decision3()
+            elif (customer1.upper() == "2"):
+                print("Alright, you may enter your order.")
+                ans = 'correct'
+                decision3()
+        elif (customer1.upper() == "5"):
+            print("Would you like a menu?\n1.Yes\n2.No")
+            customer1 = input()
+            if (customer1.upper() == "1"):
+                print("MENU5")
+                ans = 'correct'
+                decision3()
+            elif (customer1.upper() == "2"):
+                print("Alright, you may enter your order.")
+                ans = 'correct'
+                decision3()
+
+        else:
+            print("Sorry we do not understand")
+            customer1 = input()
+
+
+def decision3():
+    import time
+    time.sleep(2)
+    customer1 = input()
+    ans = 'incorrect'
+    while (ans == 'incorrect'):
+        if len(customer1.upper()) >= 1:
+            print("Thank you, we will process your order")
+            ans = 'correct'
+        else:
+            print("Sorry we do not understand")
+            customer1 = input()
+    time.sleep(2)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+decision1()
 
 
 
